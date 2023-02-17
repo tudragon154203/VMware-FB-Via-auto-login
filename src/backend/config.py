2252f7b70224613f481d9364e1310e2d42a5a1de
tudragon154203 <minhtudragon1997@gmail.com>
2023-02-17 07:56:00 +0700

    Better config

--- src/backend/config.py
+++ src/backend/config.py
@@ -17,17 +17,27 @@ class Config(UserDict):
         :param vm_root_dir: root directory of all vmware instances
         :param keyword: search for 'keyword' in all VM's names and only run these VMs
         :param t_between_sessions: time in seconds between two machine sessions. default to 5s
+        :param enable (of screenshot): do you enable taking screenshot?
         :param days_between_screenshots: time in seconds between two machine sessions. default to 5s
         :param screenshot_dir: output screenshot path. Default to "../screenshots"
         """
         data = {
-            "vm_root_dir": pathlib.Path(vm_root_dir),
-            "keyword": keyword,
-            "t_running": t_running,
-            "t_between_sessions": t_between_sessions,
-            "log_path": pathlib.Path(log_path),
-            "days_between_screenshots": days_between_screenshots,
-            "screenshot_dir": pathlib.Path(screenshot_dir)
+            "vm_filter":{
+                "vm_root_dir": pathlib.Path(vm_root_dir),
+                "keyword": keyword
+            },
+            "runtime":{
+                "t_running": t_running,
+                "t_between_sessions": t_between_sessions
+            },
+            "monitor":{
+                "log_path": pathlib.Path(log_path),
+                "screenshot":{
+                    "enable": True,
+                    "days_between_screenshots": days_between_screenshots,
+                    "screenshot_dir": pathlib.Path(screenshot_dir)      
+                }
+            }
         }
         self.file_path = pathlib.Path(file_path)
         super().__init__(data)
@@ -44,9 +54,9 @@ class Config(UserDict):
             file_path = pathlib.Path(file_path)
 
         data = self.data
-        data["vm_root_dir"] = str(self.data["vm_root_dir"].as_posix())
-        data["log_path"] = str(self.data["log_path"].as_posix())
-        data["screenshot_dir"] = str(self.data["screenshot_dir"].as_posix())
+        data["vm_filter"]["vm_root_dir"] = str(self.data["vm_filter"]["vm_root_dir"].as_posix())
+        data["monitor"]["log_path"] = str(self.data["monitor"]["log_path"].as_posix())
+        data["monitor"]["screenshot"]["screenshot_dir"] = str(self.data["monitor"]["screenshot"]["screenshot_dir"].as_posix())
 
         with open(file_path, "w") as f:
             json.dump(data, f)
@@ -65,9 +75,9 @@ class Config(UserDict):
                 self.data = config_data
 
                 # parse paths using pathlib
-                self.data["vm_root_dir"] = pathlib.Path(config_data["vm_root_dir"])
-                self.data["log_path"] = pathlib.Path(config_data["log_path"])
-                self.data["screenshot_dir"] = pathlib.Path(config_data["screenshot_dir"])
+                self.data["vm_filter"]["vm_root_dir"] = pathlib.Path(config_data["vm_filter"]["vm_root_dir"])
+                self.data["monitor"]["log_path"] = pathlib.Path(config_data["monitor"]["log_path"])
+                self.data["monitor"]["screenshot"]["screenshot_dir"] = pathlib.Path(config_data["monitor"]["screenshot"]["screenshot_dir"])
 
                 print(f'Config loaded: {config_data}')
         except: #No file_path
@@ -76,5 +86,7 @@ class Config(UserDict):
 
 if __name__ == "__main__":
     config = Config()
-    config.load_config(file_path = "config.json")
-    config.save_config(file_path = "config.json")
+    # config_file_path = "config.json"
+    config_file_path = "src/backend/config.json"
+    config.load_config(file_path = config_file_path)
+    config.save_config(file_path = config_file_path)
