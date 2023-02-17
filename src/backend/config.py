0d49e9e91df8c2e4955e3a14a707ea1052b7af65
tudragon154203 <minhtudragon1997@gmail.com>
2023-02-17 07:42:27 +0700

    refactor vm_app to use app.config

--- src/backend/config.py
+++ src/backend/config.py
@@ -13,7 +13,12 @@ class Config(UserDict):
     screenshot_dir = "../screenshots",
     file_path = "config.json"):
         """
-        Initialize Config class with default values for vm_root_dir, keyword, t_running, t_between_sessions and log_path
+        Initialize Config class with default values:
+        :param vm_root_dir: root directory of all vmware instances
+        :param keyword: search for 'keyword' in all VM's names and only run these VMs
+        :param t_between_sessions: time in seconds between two machine sessions. default to 5s
+        :param days_between_screenshots: time in seconds between two machine sessions. default to 5s
+        :param screenshot_dir: output screenshot path. Default to "../screenshots"
         """
         data = {
             "vm_root_dir": pathlib.Path(vm_root_dir),
--- src/backend/vm_app.py
+++ src/backend/vm_app.py
@@ -3,20 +3,15 @@ from config import Config
 from virtual_machine import VirtualMachine
 from vm_log_session import VMLogSession
 from logger import Logger 
+import pathlib
 
 class VMApp:
     """
     Initialize VMApp class with vm_root_dir, keyword and t_between_sessions
-    In self.config:
-    :param vm_root_dir: root directory of all vmware instances
-    :param keyword: search for 'keyword' in all VM's names and only run these VMs
-    :param t_between_sessions: time in seconds between two machine sessions. default to 5s
-    :param days_between_screenshots: time in seconds between two machine sessions. default to 5s
-    :param screenshot_dir: output screenshot path. Default to "../screenshots"
-
+    params passed through self.config:
+    
     :attr vm_log_sessions - list of log sessions
     :attr logger - log messages into "../log
-
 
     Method:
     + run(): _scan() to get list of .vmx files, then _create_sessions() to get list of VMLogSession. For each VMLogSession instance, call its run() method, wait for t_between_sessions seconds and call run() of the next one.
@@ -27,14 +22,8 @@ class VMApp:
     + _create_sessions(vmx_paths): from list of .vmx files, create VMLogSession instances and put them in a list. Return list of VMLogSession 
     """
     def __init__(self, config):
-        self.vm_root_dir = config["vm_root_dir"]
-        self.keyword = config["keyword"]
-        self.t_between_sessions = config["t_between_sessions"]
-        self.t_running = config["t_running"]
+        self.config = config
         self.logger = Logger.instance(__name__, config["log_path"])
-        self.days_between_screenshots = config["days_between_screenshots"]
-        self.screenshot_dir = config["screenshot_dir"]
-
         self.vm_log_sessions = []
         config.save_config(config.file_path)
 
@@ -53,7 +42,7 @@ class VMApp:
 
         for session in self.vm_log_sessions:
             session.run()
-            time.sleep(self.t_between_sessions)
+            time.sleep(self.config["t_between_sessions"])
             self.logger.log(f"{self.get_progress()}/{length} sessions completed")
 
     def _scan(self):
@@ -63,8 +52,9 @@ class VMApp:
         Return list of absolute paths of .vmx files (unsorted)
         """
         vmx_paths = []
-        for p in self.vm_root_dir.rglob("*"):
-            if p.is_file() and p.suffix == ".vmx" and self.keyword in str(p):
+        vm_root_dir = pathlib.Path(self.config["vm_root_dir"])
+        for p in vm_root_dir.rglob("*"):
+            if p.is_file() and p.suffix == ".vmx" and self.config["keyword"] in str(p):
                 vmx_paths.append(p)
         return vmx_paths
 
@@ -78,7 +68,9 @@ class VMApp:
             # vm_name = path.split("/")[-1]
             vm_name = path.stem
             vm = VirtualMachine(name=vm_name, vmx_file_path=path)
-            log_session = VMLogSession(virtual_machine=vm, t_running = self.t_running, take_screenshot=True) #TODO: take screenshot based on calculation
+            log_session = VMLogSession(virtual_machine=vm, 
+                                       t_running = self.config["t_running"], take_screenshot=True) 
+            #TODO: take screenshot based on calculation whether to take it
             vm_log_sessions.append(log_session)
         return vm_log_sessions
 
@@ -88,13 +80,14 @@ class VMApp:
         :return: number of completed sessions
         """
         completed_sessions = 0
-        for session in self.vm_log_sessions:
+        for session in self.config["vm_log_sessions"]:
             if session.status == VMLogSession.Status.COMPLETED:
                 completed_sessions += 1
         return completed_sessions
 
 if __name__ == "__main__":
-    config_file_path = "config.json"
+    # config_file_path = "config.json"
+    config_file_path = "src/backend/config.json"
     config = Config(file_path=config_file_path)
     config.load_config(config_file_path)
     vm_app = VMApp(config)
