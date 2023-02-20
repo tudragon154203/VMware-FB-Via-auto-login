a8ceae7b3b5aea7a6044c59eeae91e220c194f7f
“tudragon” <minhtudragon1997@gmail.com>
2023-02-20 22:53:32 +0700

    edit run.sh to unix like

--- src/backend/run.sh
+++ src/backend/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#export PATH=/home/tudragon/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin
 export DISPLAY=":0.0"
 
 cd ~/SSD/VMware\ machines/VMware\ FB\ Via\ auto\ login/src/backend
