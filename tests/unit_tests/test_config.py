dfc408eae0f28f45e76d4c557efebd366a96e757
“tudragon” <minhtudragon1997@gmail.com>
2023-02-24 13:56:24 +0700

    tox passed py3.10

--- tests/unit_tests/test_config.py
+++ tests/unit_tests/test_config.py
@@ -1,8 +1,10 @@
-from backend.config import Config 
+from backend.config import Config
+
 
 def test_defaut_config_has_t_running():
     config = Config()
-    assert (config["runtime"]["t_running"] > 0) 
+    assert (config["runtime"]["t_running"] > 0)
+
 
 def test_defaut_config_has_t_between_sessions():
     config = Config()
